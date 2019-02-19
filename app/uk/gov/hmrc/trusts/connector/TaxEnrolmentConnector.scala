8da0e599dd3db6ac8fffc029cf0a842910a05bd5
Manish Wadhwani <manish.wadhwani@digital.hmrc.gov.uk>
2019-02-19 14:12:09 +0000

    tax enrolment subscription changes.

---
+++ app/uk/gov/hmrc/trusts/connector/TaxEnrolmentConnector.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.trusts.connector
+
+import javax.inject.Inject
+
+import com.google.inject.ImplementedBy
+import play.api.Logger
+import play.api.libs.json.{JsValue, Json, Writes}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.logging.Authorization
+import uk.gov.hmrc.trusts.config.{AppConfig, WSHttp}
+import uk.gov.hmrc.trusts.models.{ExistingTrustResponse, TaxEnrolmentSubscription, TaxEnrolmentSuscriberResponse}
+import uk.gov.hmrc.trusts.utils.Contstants._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class TaxEnrolmentConnectorImpl @Inject()(http: WSHttp, config: AppConfig) extends TaxEnrolmentConnector {
+
+  def headers =
+    Seq(
+      CONTENT_TYPE -> CONTENT_TYPE_JSON
+    )
+
+  override  def enrolSubscriber(subscriptionId: String)(implicit hc: HeaderCarrier) :  Future[TaxEnrolmentSuscriberResponse] = {
+    val taxEnrolmentsEndpoint = s"${config.taxEnrolmentsUrl}/tax-enrolments/subscriptions/$subscriptionId/subscriber"
+    val taxEnolmentHeaders = hc.withExtraHeaders(headers: _*)
+
+    Logger.debug(s"Requesting the enrolment for : subscriptionId - $subscriptionId")
+
+    val taxRequest = TaxEnrolmentSubscription(
+      serviceName = config.taxEnrolmentsPayloadBodyServiceName,
+      callback = config.taxEnrolmentsPayloadBodyCallback,
+      etmpId = subscriptionId)
+
+    val response = http.PUT[JsValue, TaxEnrolmentSuscriberResponse](taxEnrolmentsEndpoint, Json.toJson(taxRequest))
+    (Writes.JsValueWrites ,TaxEnrolmentSuscriberResponse.httpReads,taxEnolmentHeaders.headers, global)
+    response
+  }
+
+}
+
+@ImplementedBy(classOf[TaxEnrolmentConnectorImpl])
+trait TaxEnrolmentConnector {
+  def enrolSubscriber(subscriptionId: String)(implicit hc: HeaderCarrier):  Future[TaxEnrolmentSuscriberResponse]
+}
---
+++ app/uk/gov/hmrc/trusts/models/TaxEnrolmentSuscriberResponse.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.trusts.models
+
+import play.api.Logger
+import play.api.http.Status.NO_CONTENT
+import play.api.libs.json.Json
+import uk.gov.hmrc.http.{HttpReads, HttpResponse}
+import uk.gov.hmrc.trusts.exceptions._
+
+
+sealed trait TaxEnrolmentSuscriberResponse
+
+object TaxEnrolmentSuscriberResponse {
+
+  implicit val formats = Json.format[TaxEnrolmentSuscriberResponse]
+
+  final case object Success extends TaxEnrolmentSuscriberResponse
+
+  final case object Failed extends TaxEnrolmentSuscriberResponse
+
+  implicit lazy val httpReads: HttpReads[TaxEnrolmentSuscriberResponse] =
+    new HttpReads[TaxEnrolmentSuscriberResponse] {
+      override def read(method: String, url: String, response: HttpResponse): TaxEnrolmentSuscriberResponse = {
+
+        Logger.info(s"[TaxEnrolmentSuscriberResponse]  response status received from tax enrolment: ${response.status}")
+        response.status match {
+          case NO_CONTENT =>
+            Success
+          case status =>
+            throw new InternalServerErrorException(s"Error response from tax enrolment:  $status")
+        }
+      }
+    } //httpReads
+}
+
