32c163bb520271b69d23e8b0f866bf3dabb1ef0c
JoeSimmonds <JoeSimmonds@users.noreply.github.com>
2020-01-28 13:16:47 +0000

    TRUS-2354: Implemented unit test for variation service.

--- test/uk/gov/hmrc/trusts/services/VariationServiceSpec.scala
+++ test/uk/gov/hmrc/trusts/services/VariationServiceSpec.scala
@@ -18,8 +18,8 @@ package uk.gov.hmrc.trusts.services
 
 import org.mockito.ArgumentCaptor
 import org.scalatest.mockito.MockitoSugar
-import org.scalatest.{ MustMatchers, WordSpec}
-import play.api.libs.json.JsValue
+import org.scalatest.{MustMatchers, WordSpec}
+import play.api.libs.json.{JsSuccess, JsValue, Json}
 import org.mockito.Mockito.{times, verify, when}
 import org.mockito.Matchers.{any, eq => equalTo}
 import org.scalatest.concurrent.ScalaFutures
@@ -28,47 +28,52 @@ import uk.gov.hmrc.trusts.models.get_trust_or_estate.ResponseHeader
 import uk.gov.hmrc.trusts.models.get_trust_or_estate.get_trust.{GetTrustSuccessResponse, TrustProcessedResponse}
 import uk.gov.hmrc.trusts.models.variation.VariationResponse
 import uk.gov.hmrc.trusts.models.{AddressType, Declaration, NameType}
+import uk.gov.hmrc.trusts.transformers.DeclareNoChangeTransformer
 import uk.gov.hmrc.trusts.utils.JsonRequests
 
 import scala.concurrent.Future
 
 class VariationServiceSpec extends WordSpec with JsonRequests with MockitoSugar with ScalaFutures with MustMatchers {
 
+  implicit  val hc: HeaderCarrier = new HeaderCarrier
+
   "Declare no change" should {
 //    "Fail if the form bundle number has changed" ignore {}
 //    "Gets Authoritative info direct from DES" ignore {}
-//    "Gets Info from Cache with deltas applied" in {
-//      val utr = "1234567890"
-//      val internalId = "InternalId"
-//      val fullEtmpResponseJson = getTrustResponse
-//      val trustInfoJson = (fullEtmpResponseJson \ "trustOrEstateDisplay").as[JsValue]
-//      val desService = mock[DesService]
-//
-//      when(desService.getTrustInfo(equalTo(utr), equalTo(internalId))(any[HeaderCarrier]())).thenReturn(Future.successful(
-//        TrustProcessedResponse(trustInfoJson, ResponseHeader("Processed", "1234567890"))
-//      ))
-//
-//      when(desService.trustVariation(equalTo(fullEtmpResponseJson))(any[HeaderCarrier])).thenReturn(Future.successful(
-//        VariationResponse("TVN34567890")
-//      ))
-//
-//      val declaration = Declaration(
-//        NameType("Handy", None, "Andy"),
-//        AddressType("Line1", "Line2", Some("Line3"), None, Some("POSTCODE"), "GB")
-//      )
-//
-//      val OUT = new VariationService()
-//
-//      whenReady(OUT.submitDeclareNoChange(utr, declaration)) {variationResponse => {
-//        variationResponse mustBe VariationResponse("TVN34567890")
-//        val arg: ArgumentCaptor[JsValue] = ArgumentCaptor.forClass(classOf[JsValue])
-//        verify(desService.trustVariation(arg.capture())(any[HeaderCarrier]), times(1))
-//        arg.getValue() mustBe fullEtmpResponseJson
-//      }}
-//    }
-//
-//    "Transforms the data as needed" ignore {
-//
-//    }
+    "Gets Info from Cache with deltas applied" in {
+      val utr = "1234567890"
+      val internalId = "InternalId"
+      val fullEtmpResponseJson = getTrustResponse
+      val trustInfoJson = (fullEtmpResponseJson \ "trustOrEstateDisplay").as[JsValue]
+      val desService = mock[DesService]
+      val auditService = mock[AuditService]
+      val transformer = mock[DeclareNoChangeTransformer]
+
+      when(desService.getTrustInfo(equalTo(utr), equalTo(internalId))(any[HeaderCarrier]())).thenReturn(Future.successful(
+        TrustProcessedResponse(trustInfoJson, ResponseHeader("Processed", "1234567890"))
+      ))
+
+      when(desService.trustVariation(any())(any[HeaderCarrier])).thenReturn(Future.successful(
+        VariationResponse("TVN34567890")
+      ))
+
+      val transformedJson = Json.obj("field" -> "value")
+      when(transformer.transform(any(),any())).thenReturn(JsSuccess(transformedJson))
+
+      val declaration = Declaration(
+        NameType("Handy", None, "Andy"),
+        AddressType("Line1", "Line2", Some("Line3"), None, Some("POSTCODE"), "GB")
+      )
+
+      val OUT = new VariationService(desService, transformer, auditService)
+
+      whenReady(OUT.submitDeclareNoChange(utr, internalId, declaration)) {variationResponse => {
+        variationResponse mustBe VariationResponse("TVN34567890")
+        verify(transformer, times(1)).transform(trustInfoJson, declaration)
+        val arg: ArgumentCaptor[JsValue] = ArgumentCaptor.forClass(classOf[JsValue])
+        verify(desService, times(1)).trustVariation(arg.capture())(any[HeaderCarrier])
+        arg.getValue mustBe transformedJson
+      }}
+    }
   }
 }
