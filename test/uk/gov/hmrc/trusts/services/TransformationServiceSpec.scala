56563b9a9e7d1e683c2b3c3bcf06763faa1cda6a
Jay Nabonne <jaynabonne@gmail.com>
2020-03-03 09:49:13 +0000

    TRUS-2389: Modified spec to not use app (runs in half the time).

--- test/uk/gov/hmrc/trusts/services/TransformationServiceSpec.scala
+++ test/uk/gov/hmrc/trusts/services/TransformationServiceSpec.scala
@@ -16,15 +16,13 @@
 
 package uk.gov.hmrc.trusts.services
 
-import java.time.LocalDate
-
 import org.joda.time.DateTime
 import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mockito.MockitoSugar
+import org.scalatest.time.{Millis, Span}
 import org.scalatest.{FreeSpec, MustMatchers}
-import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.libs.json.{JsResult, JsValue, Json}
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.trusts.models.get_trust_or_estate.get_trust._
@@ -35,7 +33,9 @@ import uk.gov.hmrc.trusts.utils.JsonUtils
 
 import scala.concurrent.Future
 
-class TransformationServiceSpec extends FreeSpec with MockitoSugar with ScalaFutures with MustMatchers with GuiceOneAppPerSuite {
+class TransformationServiceSpec extends FreeSpec with MockitoSugar with ScalaFutures with MustMatchers {
+  // Removing the usage of GuiceOneAppPerSuite started timing out a test without something like this.
+  private implicit val pc: PatienceConfig = PatienceConfig(timeout = Span(250, Millis), interval = Span(15, Millis))
 
   val newLeadTrusteeIndInfo = DisplayTrustLeadTrusteeIndType(
     lineNo = Some("newLineNo"),
@@ -90,9 +90,9 @@ class TransformationServiceSpec extends FreeSpec with MockitoSugar with ScalaFut
     entityStart = DateTime.parse("1998-02-12")
   )
 
-  val auditService = app.injector.instanceOf[FakeAuditService]
+  private val auditService = mock[FakeAuditService]
 
-  implicit val hc : HeaderCarrier = HeaderCarrier()
+  private implicit val hc : HeaderCarrier = HeaderCarrier()
 
   private val originalJson = Json.obj()
 
