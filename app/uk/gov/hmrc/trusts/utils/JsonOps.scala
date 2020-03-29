c611856f7488b247f327b8f0b33b4c4b5edfdfac
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2020-03-29 14:22:52 +0100

    Before submitting a registration or variation the data is traversed to remove brackets from phone numbers.

---
+++ app/uk/gov/hmrc/trusts/utils/JsonOps.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.trusts.utils
+
+import play.api.libs.json._
+
+object JsValueOps {
+
+  type JsPath = Seq[Either[Int,String]]
+  type JsEntry = (JsPath, JsValue)
+  type JsTraverse = PartialFunction[JsEntry, JsValue]
+
+  implicit class JsPathOps(underlying: JsPath) {
+    def isEndsWith(field: String): Boolean = underlying.lastOption.contains(Right(field))
+    def isEndsWith(index: Int): Boolean = underlying.lastOption.contains(Left(index))
+    def /(field: String): JsPath = underlying :+ Right(field)
+    def /(index: Int): JsPath = underlying :+ Left(index)
+  }
+
+  implicit class JsValueOps(underlying: JsValue) {
+    /**
+     * Traverse underlying json based on given partially defined function `f` only on scalar values, like:
+     * null, string or number.
+     *
+     * @param f function
+     * @return updated json
+     */
+    def traverse(f: JsTraverse): JsValue = {
+      def traverseRec(prefix: JsPath, value: JsValue): JsValue = {
+        val lifted: JsValue => JsValue = value => f.lift(prefix -> value).getOrElse(value)
+        value match {
+          case JsNull => lifted(JsNull)
+          case boolean: JsBoolean => lifted(boolean)
+          case number: JsNumber => lifted(number)
+          case string: JsString => lifted(string)
+          case array: JsArray =>
+            val updatedArray = array.value.zipWithIndex.map {
+              case (arrayValue, index) => traverseRec(prefix / index, arrayValue)
+            }
+            JsArray(updatedArray)
+
+          case obj: JsObject =>
+            val updatedFields = obj.fieldSet.toSeq.map {
+              case (field, fieldValue) => field -> traverseRec(prefix / field, fieldValue)
+            }
+            JsObject(updatedFields)
+        }
+      }
+      traverseRec(Nil, underlying)
+    }
+  }
+}
