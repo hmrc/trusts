001cbdece52bc18d65c28bc7927a9ccfabf868aa
Jay Nabonne <jaynabonne@gmail.com>
2020-04-01 16:12:04 +0100

    Move some integration tests to better package and add copyright headers.

--- it/uk/gov/hmrc/transformations/AmendTrusteeSpec.scala
+++ it/uk/gov/hmrc/transformations/AmendTrusteeSpec.scala
@@ -1,4 +1,20 @@
-package uk.gov.hmrc.repositories
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.transformations
 
 import org.joda.time.DateTime
 import org.mockito.Matchers._
@@ -10,45 +26,41 @@ import play.api.libs.json.{JsValue, Json}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.auth.core.AffinityGroup.Organisation
+import uk.gov.hmrc.repositories.TransformIntegrationTest
 import uk.gov.hmrc.trusts.connector.DesConnector
 import uk.gov.hmrc.trusts.controllers.actions.{FakeIdentifierAction, IdentifierAction}
-import uk.gov.hmrc.trusts.models.get_trust_or_estate.get_trust.{DisplayTrustIdentificationType, DisplayTrustLeadTrusteeIndType, GetTrustSuccessResponse}
-import uk.gov.hmrc.trusts.models.{AddressType, NameType}
+import uk.gov.hmrc.trusts.models.NameType
+import uk.gov.hmrc.trusts.models.get_trust_or_estate.get_trust.{DisplayTrustIdentificationType, DisplayTrustTrusteeIndividualType, GetTrustSuccessResponse}
 import uk.gov.hmrc.trusts.utils.JsonUtils
 
 import scala.concurrent.Future
 
-class AmendLeadTrusteeSpec extends FreeSpec with MustMatchers with MockitoSugar with TransformIntegrationTest {
+class AmendTrusteeSpec extends FreeSpec with MustMatchers with MockitoSugar with TransformIntegrationTest {
 
   val getTrustResponseFromDES: GetTrustSuccessResponse = JsonUtils.getJsonValueFromFile("trusts-etmp-received.json").as[GetTrustSuccessResponse]
   val expectedInitialGetJson: JsValue = JsonUtils.getJsonValueFromFile("trusts-integration-get-initial.json")
 
-  "an amend lead trustee call" - {
+  "an amend trustee call" - {
     "must return amended data in a subsequent 'get' call" in {
 
-      val newTrusteeIndInfo = DisplayTrustLeadTrusteeIndType(
-        lineNo = None,
+      val newTrusteeIndInfo = DisplayTrustTrusteeIndividualType(
+        lineNo = Some("newLineNo"),
         bpMatchStatus = None,
         name = NameType("newFirstName", Some("newMiddleName"), "newLastName"),
-        dateOfBirth = new DateTime(1965, 2, 10, 0, 0),
-        phoneNumber = "newPhone",
-        email = Some("newEmail"),
-        identification = DisplayTrustIdentificationType(
-          None,
-          Some("newNino"),
-          None,
-          Some(AddressType(
-            "1344 Army Road",
-            "Suite 111",
-            Some("Telford"),
-            Some("Shropshire"),
-            Some("TF1 5DR"),
-            "GB"
-          ))),
-          None
+        dateOfBirth = Some(new DateTime(1965, 2, 12, 0, 0)),
+        phoneNumber = Some("newPhone"),
+        identification = Some(
+          DisplayTrustIdentificationType(
+            None,
+            Some("newNino"),
+            None,
+            None
+          )
+        ),
+        entityStart = new DateTime(1998, 2, 12, 0, 0)
       )
 
-      val expectedGetAfterAmendLeadTrusteeJson: JsValue = JsonUtils.getJsonValueFromFile("trusts-integration-get-after-amend-lead-trustee.json")
+      val expectedGetAfterAmendTrusteeJson: JsValue = JsonUtils.getJsonValueFromFile("trusts-integration-get-after-amend-trustee.json")
 
       val stubbedDesConnector = mock[DesConnector]
       when(stubbedDesConnector.getTrustInfo(any())(any())).thenReturn(Future.successful(getTrustResponseFromDES))
@@ -67,7 +79,7 @@ class AmendLeadTrusteeSpec extends FreeSpec with MustMatchers with MockitoSugar 
           status(result) mustBe OK
           contentAsJson(result) mustBe expectedInitialGetJson
 
-          val amendRequest = FakeRequest(POST, "/trusts/amend-lead-trustee/5174384721")
+          val amendRequest = FakeRequest(POST, "/trusts/amend-trustee/5174384721/0")
             .withBody(Json.toJson(newTrusteeIndInfo))
             .withHeaders(CONTENT_TYPE -> "application/json")
 
@@ -76,11 +88,12 @@ class AmendLeadTrusteeSpec extends FreeSpec with MustMatchers with MockitoSugar 
 
           val newResult = route(application, FakeRequest(GET, "/trusts/5174384721/transformed")).get
           status(newResult) mustBe OK
-          contentAsJson(newResult) mustBe expectedGetAfterAmendLeadTrusteeJson
+          contentAsJson(newResult) mustBe expectedGetAfterAmendTrusteeJson
 
           dropTheDatabase(connection)
         }.get
       }
     }
   }
+
 }
--- it/uk/gov/hmrc/transformations/AmendUnidentifiedBeneficiarySpec.scala
+++ it/uk/gov/hmrc/transformations/AmendUnidentifiedBeneficiarySpec.scala
@@ -1,4 +1,20 @@
-package uk.gov.hmrc.repositories
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.transformations
 
 import org.mockito.Matchers._
 import org.mockito.Mockito._
@@ -10,6 +26,7 @@ import play.api.libs.json.{JsString, JsValue}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.auth.core.AffinityGroup.Organisation
+import uk.gov.hmrc.repositories.TransformIntegrationTest
 import uk.gov.hmrc.trusts.connector.DesConnector
 import uk.gov.hmrc.trusts.controllers.actions.{FakeIdentifierAction, IdentifierAction}
 import uk.gov.hmrc.trusts.models.get_trust_or_estate.get_trust.GetTrustSuccessResponse
