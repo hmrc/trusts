d53f923201f5be113861ab1264aea5a2abfe1f49
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2020-09-23 17:00:59 +0100

    TRUS-3199: Remove deprecated warnings from application.conf

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,11 +10,10 @@ object AppDependencies {
     ws,
     "org.reactivemongo"         %% "play2-reactivemongo"        % "0.18.8-play27",
     "uk.gov.hmrc"               %% "bootstrap-backend-play-27"  % "2.25.0",
-    "com.github.java-json-tools" % "json-schema-validator"      % "2.2.8"
+    "com.github.java-json-tools" % "json-schema-validator"      % "2.2.14"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "hmrctest"            % "3.9.0-play-26" % testScope,
     "org.scalatest"          %% "scalatest"           % "3.0.8" % testScope,
     "org.pegdown"            % "pegdown"              % "1.6.0" % testScope,
     "com.github.tomakehurst" % "wiremock-standalone"  % "2.25.1",
