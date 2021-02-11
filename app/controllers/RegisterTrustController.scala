18a8ec1437afd588645de2cd4b790f5ba33e1065
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-02-11 22:13:24 +0000

    Refactored register trust controller registration method into chain of functions.

--- app/controllers/RegisterTrustController.scala
+++ app/controllers/RegisterTrustController.scala
@@ -29,7 +29,7 @@ import play.api.Logging
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Action, ControllerComponents, Result}
 import services.{AuditService, RosmPatternService, TrustsService, ValidationService, _}
-import uk.gov.hmrc.http.BadRequestException
+import uk.gov.hmrc.http.{BadRequestException, HeaderCarrier}
 import utils.ErrorResponses._
 import utils.Headers
 
@@ -51,57 +51,80 @@ class RegisterTrustController @Inject()(
 
   def registration(): Action[JsValue] = identify.async(parse.json) {
     implicit request =>
-      request.headers.get(Headers.DraftRegistrationId).fold {
-        logger.error(s"[Session ID: ${request.sessionId}] no draft id provided in headers")
-        Future.successful(BadRequest(Json.toJson(noDraftIdProvided)))
-      } { draftId =>
-
-        schemaF.flatMap {
-          schema =>
-
-            val is5mldEnabled: Boolean = schema == config.trustsApiRegistrationSchema5MLD
-            val payload = amendSubmissionDataService.applyRulesAndAddSubmissionDate(is5mldEnabled, request.body)
-
-            val validation = validationService.get(schema).validate[Registration](payload.toString())
-
-            validation.fold(
-              _ => {
-                logger.error(s"[Session ID: ${request.sessionId}] problem validating submission")
-                Future.successful(invalidRequestErrorResponse)
-              },
-              v =>
-                trustsService.registerTrust(v).flatMap {
-                  response =>
-                    auditService.audit(
-                      event = TrustAuditing.TRUST_REGISTRATION_SUBMITTED,
-                      registration = v,
-                      draftId = draftId,
-                      internalId = request.internalId,
-                      response = response
-                    )
-
-                    rosmPatternService.enrolAndLogResult(
-                      trn = response.trn,
-                      affinityGroup = request.affinityGroup,
-                      taxable = v.trust.details.trustTaxable.getOrElse(true)
-                    ) map {
-                      _ =>
-                        Ok(Json.toJson(response))
-                    }
-                } recover[Result](
-                  handleBusinessErrors(v, draftId) orElse
-                    handleHttpError(v, draftId)
-                  )
-            )
-        }
-      }
+      getDraftId
   }
 
-  private def handleHttpError(v: Registration, draftId: String)(implicit request: IdentifierRequest[_]): PartialFunction[Throwable, Result] = {
+  private def getDraftId(implicit request: IdentifierRequest[JsValue]): Future[Result] = {
+    request.headers.get(Headers.DraftRegistrationId) match {
+      case Some(draftId) =>
+        determineMldVersion(draftId)
+      case _ =>
+        logger.error(s"[Session ID: ${request.sessionId}] no draft id provided in headers")
+        Future.successful(BadRequest(Json.toJson(noDraftIdProvided)))
+    }
+  }
+
+  private def determineMldVersion(draftId: String)
+                                 (implicit request: IdentifierRequest[JsValue], hc: HeaderCarrier): Future[Result] = {
+    trustsStoreService.is5mldEnabled.flatMap {
+      is5mldEnabled =>
+        amendRegistration(draftId, is5mldEnabled)
+    }
+  }
+
+  private def amendRegistration(draftId: String, is5mldEnabled: Boolean)
+                               (implicit request: IdentifierRequest[JsValue]): Future[Result] = {
+    val payload: JsValue = amendSubmissionDataService.applyRulesAndAddSubmissionDate(is5mldEnabled, request.body)
+    val schema: String = if (is5mldEnabled) {
+      config.trustsApiRegistrationSchema5MLD
+    } else {
+      config.trustsApiRegistrationSchema4MLD
+    }
+    validateRegistration(draftId, schema, payload)
+  }
+
+  private def validateRegistration(draftId: String, schema: String, data: JsValue)
+                                  (implicit request: IdentifierRequest[JsValue]): Future[Result] = {
+    validationService.get(schema).validate[Registration](data.toString()) match {
+      case Right(registration) =>
+        register(registration, draftId)
+      case Left(validationErrors) =>
+        logger.error(s"[Session ID: ${request.sessionId}] problem validating submission: $validationErrors")
+        Future.successful(invalidRequestErrorResponse)
+    }
+  }
+
+  private def register(registration: Registration, draftId: String)
+                      (implicit request: IdentifierRequest[JsValue]): Future[Result] = {
+    trustsService.registerTrust(registration).flatMap {
+      response =>
+        auditService.audit(
+          event = TrustAuditing.TRUST_REGISTRATION_SUBMITTED,
+          registration = registration,
+          draftId = draftId,
+          internalId = request.internalId,
+          response = response
+        )
+
+        rosmPatternService.enrolAndLogResult(
+          trn = response.trn,
+          affinityGroup = request.affinityGroup,
+          taxable = registration.trust.details.trustTaxable.getOrElse(true)
+        ) map { _ =>
+          Ok(Json.toJson(response))
+        }
+    } recover[Result] {
+      handleBusinessErrors(registration, draftId) orElse
+        handleHttpError(registration, draftId)
+    }
+  }
+
+  private def handleHttpError(registration: Registration, draftId: String)
+                             (implicit request: IdentifierRequest[_]): PartialFunction[Throwable, Result] = {
     case _: ServiceNotAvailableException =>
       auditService.audit(
         event = TrustAuditing.TRUST_REGISTRATION_SUBMITTED,
-        registration = v,
+        registration = registration,
         draftId = draftId,
         internalId = request.internalId,
         response = RegistrationFailureResponse(SERVICE_UNAVAILABLE, "SERVICE_UNAVAILABLE", "Dependent systems are currently not responding.")
@@ -112,7 +135,7 @@ class RegisterTrustController @Inject()(
     case _: BadRequestException =>
       auditService.audit(
         event = TrustAuditing.TRUST_REGISTRATION_SUBMITTED,
-        registration = v,
+        registration = registration,
         draftId = draftId,
         internalId = request.internalId,
         response = RegistrationFailureResponse(BAD_REQUEST, "INVALID_PAYLOAD", "Submission has not passed validation. Invalid payload.")
@@ -125,11 +148,12 @@ class RegisterTrustController @Inject()(
       InternalServerError(Json.toJson(internalServerErrorResponse))
   }
 
-  def handleBusinessErrors(v: Registration, draftId: String)(implicit request: IdentifierRequest[_]): PartialFunction[Throwable, Result] = {
+  def handleBusinessErrors(registration: Registration, draftId: String)
+                          (implicit request: IdentifierRequest[_]): PartialFunction[Throwable, Result] = {
     case AlreadyRegisteredException =>
       auditService.audit(
         event = TrustAuditing.TRUST_REGISTRATION_SUBMITTED,
-        registration = v,
+        registration = registration,
         draftId = draftId,
         internalId = request.internalId,
         response = RegistrationFailureResponse(FORBIDDEN, "ALREADY_REGISTERED", "Trust is already registered.")
@@ -140,20 +164,13 @@ class RegisterTrustController @Inject()(
     case NoMatchException =>
       auditService.audit(
         event = TrustAuditing.TRUST_REGISTRATION_SUBMITTED,
-        registration = v,
+        registration = registration,
         draftId = draftId,
         internalId = request.internalId,
         response = RegistrationFailureResponse(FORBIDDEN, "NO_MATCH", "There is no match in HMRC records.")
       )
       logger.info(s"[registration][Session ID: ${request.sessionId}] Returning no match response.")
       Forbidden(Json.toJson(noMatchRegistrationResponse))
-  }
-
-  private def schemaF(implicit request: IdentifierRequest[JsValue]): Future[String] = {
-    trustsStoreService.is5mldEnabled.map {
-      case true => config.trustsApiRegistrationSchema5MLD
-      case _ => config.trustsApiRegistrationSchema4MLD
-    }
   }
 
 }
