607a37f253c01648fd248467cfe6284379557a7f
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-05-20 21:37:37 +0100

    Reverting earlier change in application.conf. Everything seems to have broken since BackendFilters were removed.

--- app/controllers/SubmissionDraftController.scala
+++ app/controllers/SubmissionDraftController.scala
@@ -43,6 +43,7 @@ class SubmissionDraftController @Inject()(submissionRepository: RegistrationSubm
 
   def setSection(draftId: String, sectionKey: String): Action[JsValue] = identify.async(parse.json) {
     implicit request => {
+      println("***")
       request.body.validate[RegistrationSubmissionDraftData] match {
         case JsSuccess(draftData, _) =>
           submissionRepository.getDraft(draftId, request.internalId).flatMap(
