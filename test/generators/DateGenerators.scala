5981731f01e746c6551e98b5b3c950095a826b11
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-06-15 11:41:30 +0100

    Added tax year service.

---
+++ test/generators/DateGenerators.scala
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package generators
+
+import org.scalacheck.{Arbitrary, Gen}
+import uk.gov.hmrc.time.TaxYear
+
+import java.time.{LocalDate, MonthDay}
+import scala.language.implicitConversions
+
+trait DateGenerators {
+
+  val arbitraryFinishYear: Int = 2021
+  val arbitraryStartYear: Int = arbitraryFinishYear - 1
+  val TAX_YEAR_START_MONTH: Int = TaxYear(arbitraryStartYear).starts.getMonthValue
+  val TAX_YEAR_START_DAY: Int = TaxYear(arbitraryStartYear).starts.getDayOfMonth
+
+  implicit lazy val arbitraryDateInTaxYear: Arbitrary[LocalDate] = {
+    arbitraryDateInTaxYearNTaxYearsAgo(0)
+  }
+
+  implicit lazy val arbitraryDateInTaxYearOnOrBeforeOctober5th: Arbitrary[LocalDate] = {
+    Arbitrary {
+      for {
+        month <- Gen.choose(TAX_YEAR_START_MONTH, 10)
+        day <- Gen.choose(
+          min = month match {
+            case TAX_YEAR_START_MONTH => TAX_YEAR_START_DAY
+            case _ => 1
+          },
+          max = month match {
+            case 4 | 6 | 9 | 11 => 30
+            case 10 => 5
+            case _ => 31
+          }
+        )
+      } yield {
+        LocalDate.of(arbitraryStartYear, month, day)
+      }
+    }
+  }
+
+  implicit lazy val arbitraryDateInTaxYearAfterOctober5th: Arbitrary[LocalDate] = {
+    Arbitrary {
+      for {
+        month <- Gen.oneOf((1 to TAX_YEAR_START_MONTH) ++ (10 to 12))
+        day <- Gen.choose(
+          min = month match {
+            case 10 => 6
+            case _ => 1
+          },
+          max = month match {
+            case 2 => 28
+            case TAX_YEAR_START_MONTH => TAX_YEAR_START_DAY - 1
+            case 4 | 6 | 9 | 11 => 30
+            case _ => 31
+          }
+        )
+      } yield {
+        val year = if (MonthDay.of(month, day).isBefore(MonthDay.of(TAX_YEAR_START_MONTH, TAX_YEAR_START_DAY))) {
+          arbitraryFinishYear
+        } else {
+          arbitraryFinishYear - 1
+        }
+        LocalDate.of(year, month, day)
+      }
+    }
+  }
+
+  implicit lazy val arbitraryDateInTaxYearOnOrBeforeDecember22nd: Arbitrary[LocalDate] = {
+    Arbitrary {
+      for {
+        month <- Gen.choose(TAX_YEAR_START_MONTH, 12)
+        day <- Gen.choose(
+          min = month match {
+            case TAX_YEAR_START_MONTH => TAX_YEAR_START_DAY
+            case _ => 1
+          },
+          max = month match {
+            case 4 | 6 | 9 | 11 => 30
+            case 12 => 22
+            case _ => 31
+          }
+        )
+      } yield {
+        LocalDate.of(arbitraryStartYear, month, day)
+      }
+    }
+  }
+
+  implicit lazy val arbitraryDateInTaxYearAfterDecember22nd: Arbitrary[LocalDate] = {
+    Arbitrary {
+      for {
+        month <- Gen.oneOf((1 to TAX_YEAR_START_MONTH) :+ 12)
+        day <- Gen.choose(
+          min = month match {
+            case 12 => 23
+            case _ => 1
+          },
+          max = month match {
+            case 2 => 28
+            case TAX_YEAR_START_MONTH => TAX_YEAR_START_DAY - 1
+            case 4 | 6 | 9 | 11 => 30
+            case _ => 31
+          }
+        )
+      } yield {
+        val year = if (MonthDay.of(month, day).isBefore(MonthDay.of(TAX_YEAR_START_MONTH, TAX_YEAR_START_DAY))) {
+          arbitraryFinishYear
+        } else {
+          arbitraryFinishYear - 1
+        }
+        LocalDate.of(year, month, day)
+      }
+    }
+  }
+
+  implicit def arbitraryDateInTaxYearNTaxYearsAgo(n: Int): Arbitrary[LocalDate] = {
+    Arbitrary {
+      for {
+        month <- Gen.choose(1, 12)
+        day <- Gen.choose(
+          min = 1,
+          max = month match {
+            case 2 => 28
+            case 4 | 6 | 9 | 11 => 30
+            case _ => 31
+          }
+        )
+      } yield {
+        val year = if (MonthDay.of(month, day).isBefore(MonthDay.of(TAX_YEAR_START_MONTH, TAX_YEAR_START_DAY))) {
+          arbitraryFinishYear - n
+        } else {
+          arbitraryFinishYear - n - 1
+        }
+        LocalDate.of(year, month, day)
+      }
+    }
+  }
+
+}
---
+++ test/services/TaxYearServiceSpec.scala
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import base.BaseSpec
+import generators.DateGenerators
+import models.FirstTaxYearAvailable
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{reset, when}
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.matchers.must.Matchers._
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import uk.gov.hmrc.time.TaxYear
+
+import java.time.LocalDate
+
+class TaxYearServiceSpec extends BaseSpec with ScalaCheckPropertyChecks with MockitoSugar with DateGenerators with BeforeAndAfterEach {
+
+  val taxYearService: TaxYearService = mock[TaxYearService]
+
+  override def beforeEach(): Unit = {
+    reset(taxYearService)
+    when(taxYearService.currentTaxYear).thenReturn(TaxYear(arbitraryStartYear))
+    when(taxYearService.firstTaxYearAvailable(any())).thenCallRealMethod()
+  }
+
+  "TaxYearService" when {
+
+    "firstTaxYearAvailable" must {
+
+      "return first tax year available for start date" when {
+
+        "before deadline" when {
+
+          "start date is more than 4 tax years ago" in {
+
+            forAll(
+              arbitrary[LocalDate](arbitraryDateInTaxYearOnOrBeforeDecember22nd),
+              Gen.choose(5, 10) // any number of years ago over 4 (10 is arbitrary)
+            ) {
+              (currentDate, yearsAgo) =>
+
+                forAll(arbitrary[LocalDate](arbitraryDateInTaxYearNTaxYearsAgo(yearsAgo))) { startDate =>
+                  beforeEach()
+
+                  when(taxYearService.currentDate).thenReturn(currentDate)
+
+                  val result = taxYearService.firstTaxYearAvailable(startDate)
+
+                  result mustBe FirstTaxYearAvailable(yearsAgo = 4, earlierYearsToDeclare = true)
+                }
+            }
+          }
+
+          "start date is 4 or less years ago" in {
+
+            forAll(
+              arbitrary[LocalDate](arbitraryDateInTaxYearOnOrBeforeDecember22nd),
+              Gen.choose(0, 4) // 4 or less years ago
+            ) {
+              (currentDate, yearsAgo) =>
+
+                forAll(arbitrary[LocalDate](arbitraryDateInTaxYearNTaxYearsAgo(yearsAgo))) { startDate =>
+                  beforeEach()
+
+                  when(taxYearService.currentDate).thenReturn(currentDate)
+
+                  val result = taxYearService.firstTaxYearAvailable(startDate)
+
+                  result mustBe FirstTaxYearAvailable(yearsAgo = yearsAgo, earlierYearsToDeclare = false)
+                }
+            }
+          }
+        }
+
+        "after deadline" when {
+
+          "start date is more than 3 tax years ago" in {
+
+            forAll(
+              arbitrary[LocalDate](arbitraryDateInTaxYearAfterDecember22nd),
+              Gen.choose(4, 10) // any number of years ago over 3 (10 is arbitrary)
+            ) {
+              (currentDate, yearsAgo) =>
+
+                forAll(arbitrary[LocalDate](arbitraryDateInTaxYearNTaxYearsAgo(yearsAgo))) { startDate =>
+                  beforeEach()
+
+                  when(taxYearService.currentDate).thenReturn(currentDate)
+
+                  val result = taxYearService.firstTaxYearAvailable(startDate)
+
+                  result mustBe FirstTaxYearAvailable(yearsAgo = 3, earlierYearsToDeclare = true)
+                }
+            }
+          }
+
+          "start date is 3 or less years ago" in {
+
+            forAll(
+              arbitrary[LocalDate](arbitraryDateInTaxYearAfterDecember22nd),
+              Gen.choose(0, 3) // 3 or less years ago
+            ) {
+              (currentDate, yearsAgo) =>
+
+                forAll(arbitrary[LocalDate](arbitraryDateInTaxYearNTaxYearsAgo(yearsAgo))) { startDate =>
+                  beforeEach()
+
+                  when(taxYearService.currentDate).thenReturn(currentDate)
+
+                  val result = taxYearService.firstTaxYearAvailable(startDate)
+
+                  result mustBe FirstTaxYearAvailable(yearsAgo = yearsAgo, earlierYearsToDeclare = false)
+                }
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
