f5529f53fdfad7cb396552ab3b2b79e1d04d0f65
Adam Conder <adamdconder@gmail.com>
2021-10-18 15:54:51 +0100

    TRUS-4666 Adding json writes for non-tax non-rep event

--- test/models/nonRepudiation/NRSSubmissionSpec.scala
+++ test/models/nonRepudiation/NRSSubmissionSpec.scala
@@ -69,6 +69,92 @@ class NRSSubmissionSpec extends BaseSpec {
           |""".stripMargin)
     }
 
+    "write to json for a taxable variation" in {
+      val payLoad = NRSSubmission(
+        "payload",
+        MetaData(businessId = "trs",
+          notableEvent = "trs-taxable-update",
+          payloadContentType = "application/json",
+          payloadSha256Checksum = "1cbdeb2d2b003b4d4d639af4bd2e1913f591f74c33940d97fd6a626161c20b67",
+          userSubmissionTimestamp = LocalDateTime.of(2020, 10, 18, 14, 10, 0, 0),
+          identityData = Json.obj("internalId" -> "some-id"),
+          userAuthToken = "AbCdEf123456",
+          headerData = Json.obj(
+            "Gov-Client-Public-IP" -> "198.51.100.0",
+            "Gov-Client-Public-Port" -> "12345"
+          ),
+          searchKeys = SearchKeys(SearchKey.UTR, "1234567890")
+        ))
+
+      Json.toJson(payLoad) mustBe Json.parse(
+        """
+          |{
+          | "payload": "payload",
+          | "metadata": {
+          |   "businessId": "trs",
+          |   "notableEvent": "trs-taxable-update",
+          |   "payloadContentType": "application/json",
+          |   "payloadSha256Checksum": "1cbdeb2d2b003b4d4d639af4bd2e1913f591f74c33940d97fd6a626161c20b67",
+          |   "userSubmissionTimestamp": "2020-10-18T14:10:00.000Z",
+          |   "identityData": {
+          |     "internalId": "some-id"
+          |   },
+          |   "userAuthToken": "AbCdEf123456",
+          |   "headerData": {
+          |     "Gov-Client-Public-IP": "198.51.100.0",
+          |     "Gov-Client-Public-Port": "12345"
+          |   },
+          |   "searchKeys": {
+          |     "utr": "1234567890"
+          |   }
+          | }
+          |}
+          |""".stripMargin)
+    }
+
+    "write to json for a non-taxable variation" in {
+      val payLoad = NRSSubmission(
+        "payload",
+        MetaData(businessId = "trs",
+          notableEvent = "trs-non-taxable-update",
+          payloadContentType = "application/json",
+          payloadSha256Checksum = "1cbdeb2d2b003b4d4d639af4bd2e1913f591f74c33940d97fd6a626161c20b67",
+          userSubmissionTimestamp = LocalDateTime.of(2020, 10, 18, 14, 10, 0, 0),
+          identityData = Json.obj("internalId" -> "some-id"),
+          userAuthToken = "AbCdEf123456",
+          headerData = Json.obj(
+            "Gov-Client-Public-IP" -> "198.51.100.0",
+            "Gov-Client-Public-Port" -> "12345"
+          ),
+          searchKeys = SearchKeys(SearchKey.URN, "ABTRUST12345678")
+        ))
+
+      Json.toJson(payLoad) mustBe Json.parse(
+        """
+          |{
+          | "payload": "payload",
+          | "metadata": {
+          |   "businessId": "trs",
+          |   "notableEvent": "trs-non-taxable-update",
+          |   "payloadContentType": "application/json",
+          |   "payloadSha256Checksum": "1cbdeb2d2b003b4d4d639af4bd2e1913f591f74c33940d97fd6a626161c20b67",
+          |   "userSubmissionTimestamp": "2020-10-18T14:10:00.000Z",
+          |   "identityData": {
+          |     "internalId": "some-id"
+          |   },
+          |   "userAuthToken": "AbCdEf123456",
+          |   "headerData": {
+          |     "Gov-Client-Public-IP": "198.51.100.0",
+          |     "Gov-Client-Public-Port": "12345"
+          |   },
+          |   "searchKeys": {
+          |     "urn": "ABTRUST12345678"
+          |   }
+          | }
+          |}
+          |""".stripMargin)
+    }
+
   }
 
 }
