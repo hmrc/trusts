3d4724551225677ec4a3d51697c0c2ccc621e56a
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2023-09-07 12:23:42 +0100

    DDCE-4530: Minor change as part of sm2 upgrade (#408)
    * DDCE-4530: Minor change as part of sm2 upgrade
    
    * DDCE-4530: Sm2 updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,7 +14,7 @@ object AppDependencies {
     "com.github.java-json-tools"    % "json-schema-validator"       % "2.2.14",
     "uk.gov.hmrc"                   %% "tax-year"                   % "3.3.0",
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.15.2",
-    "commons-codec"                 % "commons-codec"               % "1.15",
+    "commons-codec"                 % "commons-codec"               % "1.16.0",
     "org.typelevel"                 %% "cats-core"                  % "2.10.0"
   )
 
@@ -25,7 +25,7 @@ object AppDependencies {
     "org.scalatestplus"             %% "scalacheck-1-17"            % "3.2.16.0",
     "org.scalatestplus"             %% "mockito-4-6"                % "3.2.15.0",
     "org.scalatestplus.play"        %% "scalatestplus-play"         % "5.1.0",
-    "com.github.tomakehurst"        % "wiremock-standalone"         % "2.27.2",
+    "org.wiremock"                  % "wiremock-standalone"         % "3.0.1",
     "com.vladsch.flexmark"          % "flexmark-all"                % "0.64.8",
     "com.typesafe.play"             %% "play-test"                  % PlayVersion.current
   ).map(_ % "test, it")
