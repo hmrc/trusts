82901db4fe08186eb6953e02a960022f3084dba6
adam-dye <31273792+adam-dye@users.noreply.github.com>
2026-08-20 11:07:21 +0100

    fix: upgraded to warn based on PR feedback.

--- app/utils/JsonNodeRenamer.scala
+++ app/utils/JsonNodeRenamer.scala
@@ -53,9 +53,9 @@ object JsonNodeRenamer {
       case Success(v) =>
         v
       case Failure(_) =>
-        logger.info(
-          s"$dotNotatationPath not found when trying to rename target to $newName, probably the field is not required" +
-            s"check your use case to see if you think it should be there"
+        logger.warn(
+          s"$dotNotatationPath not found when trying to rename target to $newName, probably the field is not required." +
+            s"Check your use case to see if you think it should be there"
         )
         in
     }
